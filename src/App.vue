ba99a7d02fc64e60298c3b63d089f6845e092f27
sunyingdi <sunyingdi519@163.com>
2023-12-25 14:33:01 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <a-button @click="onClick">点击次数：{{ count }}</a-button>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  import HelloWorld from './components/HelloWorld.vue'
+
+  const count = ref(0);
+  const onClick = () => {
+    count.value ++;
+  };
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
